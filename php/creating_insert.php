3447d348c81c7fdd7722490035b4af33cb6874e8
Jonathan Jia <jonathan.ancc@gmail.com>
2019-05-23 10:37:44 +1000

    update

--- php/creating_insert.php
+++ php/creating_insert.php
@@ -3,9 +3,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
-//include_once 'customer_info.php';
-
 header('Content-Type', 'application/json');
 
 function query_information($con, $obj, $purpose, $table_name, $db_name) {
